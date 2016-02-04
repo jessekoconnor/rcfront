d78b0f2f9cef4a7d6244266653d63867983c624d
Jesse O'Connor <jesse.oconnor@meltwater.com>
2016-02-03 22:21:23 -0500

    Cross Origin Resource Sharing (CORS) is implemented

--- RoleCall/www/js/index.js
+++ RoleCall/www/js/index.js
@@ -48,28 +48,13 @@ var app = {
     }
 };
 
-function httpGetAsync(theUrl, callback){
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() {  
-            console.log('Refresh has responded with code: ', xmlHttp.status);
-
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-            callback(xmlHttp.responseText);
-    }
-
-    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
-    xmlHttp.send(null);
-}
-
 app.initialize();
 
 $(document).ready(function() {
 
     // Jahsee's refresh button
     $('#refreshButton').click(function() {
-        httpGetAsync('http://localhost:5000/cool', function() {
-            console.log('***** REFRESH SUCCESS - YEWWWW *********')
-        });
+        makeCorsRequest('http://rollcall.local:5000/cool');
     });
 
     // highlight current nav item
@@ -92,3 +77,55 @@ $(document).ready(function() {
         window.location.href = "/self.html";
     });
 });
+
+
+
+////////////////////////////
+// Cross origin request shit
+////////////////////////////
+
+// Create the XHR object.
+function createCORSRequest(method, url) {
+    var xhr = new XMLHttpRequest();
+    if ("withCredentials" in xhr) {
+        // XHR for Chrome/Firefox/Opera/Safari.
+        xhr.open(method, url, true);
+    } else if (typeof XDomainRequest != "undefined") {
+        // XDomainRequest for IE.
+        xhr = new XDomainRequest();
+        xhr.open(method, url);
+    } else {
+        // CORS not supported.
+        xhr = null;
+    }
+    return xhr;
+}
+
+// Helper method to parse the title tag from the response.
+function getTitle(text) {
+    return text.match('<title>(.*)?</title>')[1];
+}
+
+// Make the actual CORS request.
+function makeCorsRequest(url) {
+    // All HTML5 Rocks properties support CORS.
+
+    var xhr = createCORSRequest('GET', url);
+    if (!xhr) {
+        alert('CORS not supported');
+        return;
+    }
+
+    // Response handlers.
+    xhr.onload = function() {
+        var text = xhr.responseText;
+        //var title = getTitle(text);
+        alert('Response from CORS request to ' + url + ': ' + text);
+    };
+
+    xhr.onerror = function() {
+        alert('Woops, there was an error making the request.');
+    };
+
+    xhr.send();
+}
